97343efcda420c8a4ea0b866ddb06f51fc3930c3
notdeckvrd <deckvrd@gmail.com>
2021-04-20 10:54:31 -0400

    feat: log checking
    Check if the log file exists and display to console.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "entry.h"
 
 using std::cout;
+using std::ifstream;
 
 int main(){
 	Entry e;
 	e.print();
-	e.createLog();
+	ifstream ifile;
+	ifile.open("log.txt");
+	if(ifile){
+		cout << "A log file exists...\n";
+	} else {
+		cout << "A log file does not exist...\n";
+	}
+	//e.createLog();
 	return 0;
 }
